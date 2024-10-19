e54729c19c8d5a85326aff63f6764fbd157b2aba
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-10-19 15:43:35 +0200

    fix Layout

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,10 +1,9 @@
-import { Outlet } from "react-router-dom";
 import AppBar from "../AppBar/AppBar";
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
     <div>
       <AppBar />
-      <Outlet />
+      {children}
     </div>
   );
 };
